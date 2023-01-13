e19255278994017e4a8d9d1ee7783c19b835f190
aaron-456 <aaronburbanoms15@gmail.com>
2023-01-13 10:44:05 -0500

    ecommers__final

---
+++ assets/script/darkmode.js
@@ -0,0 +1,9 @@
+
+const sun =  document.querySelector(".buton");
+
+sun.addEventListener('click', function(){
+    sun.classList.toggle("sun_active");
+    document.body.classList.toggle("darkmode")
+});
+
+
